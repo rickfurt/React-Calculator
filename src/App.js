b00c29ff4e7c8a9127b4aef1174354cb5838ec96
rickfurt <ricardofurtado2609@gmail.com>
2019-03-16 00:38:30 +1100

    Visual Interface created

--- src/App.js
+++ src/App.js
@@ -4,9 +4,31 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Test</h1>
-      </div>
+      <div className="app-container">
+        <div className="display">
+          <div className="preview">12312312313</div>
+        </div>
+        <div className="operators">
+          <button className="operator">+</button>
+          <button className="operator">-</button>
+          <button className="operator">÷</button>
+          <button className="operator">*</button>
+        </div>
+        <div className="numbers">
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+          <button className="number">4</button>
+          <button className="number">5</button>
+          <button className="number">6</button>
+          <button className="number">7</button>
+          <button className="number">8</button>
+          <button className="number">9</button>
+          <button className="clear">AC</button>
+          <button className="number">0</button>
+          <button className="operator">=</button>
+        </div>
+    </div>
     );
   }
 }
